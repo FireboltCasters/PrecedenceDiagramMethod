4238921101ac20d07745511fb38828e6144fff14
Nils <nilsbaumgartner1994@gmail.com>
2022-05-24 18:16:39 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
 import {PrecedenceGraph} from "./api/src";
 
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";
 import {Button} from "primereact/button";                                //icons
+import { Toolbar } from 'primereact/toolbar';
+import {SplitButton} from "primereact/splitbutton";
+import Flow from "./flow/Flow";
 
 let graph = new PrecedenceGraph({});
 
@@ -15,26 +16,40 @@ function App() {
 
   const [counter, setCounter] = useState(0)
 
+  function renderToolbar(){
+    const leftContents = (
+        <React.Fragment>
+          <Button label="New" icon="pi pi-plus" className="mr-2" style={{margin: 5}} />
+          <Button label="Import" icon="pi pi-upload" className="p-button-success" style={{margin: 5}} />
+          <SplitButton label="Export" icon="pi pi-download" className="p-button-warning" style={{margin: 5}} />
+        </React.Fragment>
+    );
+
+    const rightContents = (
+        <React.Fragment>
+          <Button icon="pi pi-search" className="mr-2" style={{margin: 5}} />
+          <Button icon="pi pi-calendar" className="p-button-success mr-2" style={{margin: 5}} />
+          <Button icon="pi pi-times" className="p-button-danger" style={{margin: 5}} />
+        </React.Fragment>
+    );
+
+    return <Toolbar left={leftContents} right={rightContents} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {graph.test()}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <p>
-        {"counter: "+counter}
-      </p>
-      <Button label="Show" onClick={() => setCounter(counter+1)} />
+    <div style={{width: "100%", height: "100vh"}}>
+       {renderToolbar()}
+        <div style={{display: "flex", flexDirection: "row"}}>
+            <div style={{display: "flex", flex: 3, backgroundColor: "orange"}}>
+                HI
+            </div>
+            <div style={{display: "flex", flex: 1, flexDirection: "column", backgroundColor: "red"}}>
+                <div>HI</div>
+                <div>HI</div>
+                <div>HI</div>
+            </div>
+        </div>
+       <Flow />
     </div>
   );
 }
